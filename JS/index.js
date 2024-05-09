e9086111f80d6380cec126c528bb98ea9dc2833b
Tahesha <taheshao@gmail.com>
2024-05-09 13:35:31 -0400

    Step 2: Create a recursive function and have that function to call itself recursively.

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,8 @@
 // Step 1: Declare a global counter variable
 let counter = 0;
+
+// Step 2: Create a recursive function
+function recursiveFunction() {
+    counter++;
+    recursiveFunction(); // Call itself recursively
+}
